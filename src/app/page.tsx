78f4d33a215c965b30069177e509091813351d25
Abhishek Ajmera <113019974+Abhi-Ajmera@users.noreply.github.com>
2025-05-18 00:32:27 +0530

    feature: create routes (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Button variant={'outline'} type="submit">
-      Hello
-    </Button>
+    <div className="starry flex h-[100dvh] w-full flex-col items-center justify-center">
+      <div className="flex min-h-60 min-w-1/3 flex-col justify-center bg-black text-center text-white">
+        <h1 className="mb-8 text-2xl font-semibold">Repair shop</h1>
+        <div className="space-y-1">
+          <div className="flex justify-center gap-4">
+            <p>Contact No. -</p>
+            <p>8777777777</p>
+          </div>
+          <div className="flex justify-center gap-4">
+            <p>Address -</p>
+            <p>Sodala, Jaipur</p>
+          </div>
+          <Link href={'https://maps.app.goo.gl/aAosZy8vh5GdAXxa7'} className="underline-offset-2 hover:underline">
+            Open in Maps
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
